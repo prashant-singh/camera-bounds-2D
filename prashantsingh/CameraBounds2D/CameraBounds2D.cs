1b4a12840c44b3d356c639c186cfd243c990bf27
Prashant Singh <hi@prashantsingh.xyz>
2019-08-09 17:21:12 +0530

    Initial commit

---
+++ prashantsingh/CameraBounds2D/CameraBounds2D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraBounds2D : MonoBehaviour
+{
+    public float boundX;
+    public float boundY;
+
+    public Vector2 maxXlimit;
+    public Vector2 maxYlimit;
+
+    Camera _camera;
+
+    public void Initialize(Camera temp_camera)
+    {
+        _camera = temp_camera;
+        CalculateBounds();
+    }
+
+    public void CalculateBounds()
+    {
+        float cameraHalfWidth = _camera.aspect * _camera.orthographicSize;
+        maxXlimit = new Vector2((transform.position.x - boundX) + cameraHalfWidth, (transform.position.x + boundX) - cameraHalfWidth);
+        maxYlimit = new Vector2((transform.position.y - boundY) + _camera.orthographicSize, (transform.position.y + boundY) - _camera.orthographicSize);
+    }
+}
---
+++ prashantsingh/CameraBounds2D/Editor/CameraBounds2DEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraBounds2D))]
+public class CameraBounds2DEditor : Editor
+{
+    CameraBounds2D _bounds;
+
+    public void OnSceneGUI()
+    {
+        _bounds = (CameraBounds2D)target;
+
+        Vector3[] verts = new Vector3[] {
+            new Vector3(_bounds.transform.position.x+_bounds.boundX,_bounds.transform.position.y+_bounds.boundY),
+            new Vector3(_bounds.transform.position.x-_bounds.boundX,_bounds.transform.position.y+_bounds.boundY),
+            new Vector3(_bounds.transform.position.x-_bounds.boundX,_bounds.transform.position.y-_bounds.boundY),
+            new Vector3(_bounds.transform.position.x+_bounds.boundX,_bounds.transform.position.y-_bounds.boundY)
+        };
+
+        Handles.DrawSolidRectangleWithOutline(verts, new Color(1, 1, 1, 0.1f), Color.yellow);
+
+        _bounds.boundX = Handles.ScaleValueHandle(_bounds.boundX, new Vector3(verts[0].x, _bounds.transform.position.y, _bounds.transform.position.z), Quaternion.identity, 3, Handles.CubeCap, 0.1f);
+        _bounds.boundX = Handles.ScaleValueHandle(-_bounds.boundX, new Vector3(verts[1].x, _bounds.transform.position.y, _bounds.transform.position.z), Quaternion.identity, 3, Handles.CubeCap, 0.1f);
+        _bounds.boundY = Handles.ScaleValueHandle(_bounds.boundY, new Vector3(_bounds.transform.position.x, verts[0].y, _bounds.transform.position.z), Quaternion.identity, 3, Handles.CubeCap, 0.1f);
+        _bounds.boundY = Handles.ScaleValueHandle(-_bounds.boundY, new Vector3(_bounds.transform.position.x, verts[2].y, _bounds.transform.position.z), Quaternion.identity, 3, Handles.CubeCap, 0.1f);
+
+
+    }
+}
